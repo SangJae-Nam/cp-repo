ef54430a31d2d4aa28901f29ec106517cea3dc3e
Sangjae Nam <sangjae.nam@lge.com>
2025-05-16 13:19:48 +0900

    Generate tree test case

--- generate_tc.cpp
+++ generate_tc.cpp
@@ -22,6 +22,54 @@ using namespace std;
 random_device rd;
 mt19937 gen(rd());
 
+void printTree(int nNumbers, int startNode, int rootNode = -1, char delim = ' ')
+{
+	uniform_int_distribution<int> disNode(0, nNumbers - 1);
+
+	int rootIdx;
+	vector<bool> used(nNumbers);
+	if (rootNode == -1) {
+		rootIdx = disNode(gen);
+		rootNode = rootIdx + startNode;
+	}
+	else {
+		rootIdx = rootNode - startNode;
+	}
+
+	queue<pair<int, int>> q; //cur, prev
+	q.push(make_pair(rootIdx, -1));
+	used[rootIdx] = true;
+	int numUsed = 1;
+
+	while (!q.empty()) {
+		auto c = q.front();
+		q.pop();
+		int &cur = c.first;
+		int &prev = c.second;
+
+		if (prev != -1) {
+			cout << cur + startNode << delim << prev + startNode << '\n';
+		}
+
+		if (numUsed < nNumbers) {
+			int numChildren = uniform_int_distribution<int>(1, nNumbers - numUsed)(gen);
+			if (numChildren > 5) {
+				numChildren /= 5;
+			}
+			for (int i = 0; i < numChildren; i++) {
+				int next = disNode(gen);
+				while (used[next]) {
+					next = disNode(gen);
+				}
+
+				used[next] = true;
+				numUsed++;
+				q.push(make_pair(next, cur));
+			}
+		}
+	}
+}
+
 void printRandomNumbers(int nNumbers, int min, int max, char delim = ' ')
 {
 	uniform_int_distribution<int> dis(min, max);
@@ -38,16 +86,17 @@ int main(int argc, char *argv[])
 	cout.tie(nullptr);
 
 	int nTestcases = 1;
-	cout << nTestcases << endl;
+	//cout << nTestcases << endl;
 	while (nTestcases--) {
-		uniform_int_distribution<int> dis(3, 20);
+		uniform_int_distribution<int> dis(2, 20);
 		int N = dis(gen);
-		int M = dis(gen);
-		cout << N << ' ' << M << endl;
-		printRandomNumbers(N, 0, 10);
-		printRandomNumbers(M, 1, 5);
-		printRandomNumbers(M, 1, N);
-		printRandomNumbers(M, 1, 10);
+		uniform_int_distribution<int> dis2(1, N - 1);
+		int M = dis2(gen);
+		uniform_int_distribution<int> dis3(1, N);
+		int P = dis3(gen);
+
+		cout << N << ' ' << M << ' ' << P << endl;
+		printTree(N, 1, P);
 	}
 
 	return 0;
