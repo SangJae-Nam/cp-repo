af12f396280756d8ace8a3f8a26119b7a16d9412
Sangjae Nam <sjnam07@gmail.com>
2025-12-11 23:40:12 +0900

    Add problem

---
+++ 17137.cpp
@@ -0,0 +1,217 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <functional>
+#include <vector>
+#include <cassert>
+#include <cstring>
+
+using namespace std;
+
+// modint
+// modint<1000000007, int> b(1000000000);
+template<int m, typename T6>
+class modint {
+protected:
+	unsigned int mVal;
+
+	static constexpr unsigned int umod() {
+		return m;
+	}
+
+public:
+	static constexpr int mod() {
+		return m;
+	}
+
+	modint (T6 v = 0) {
+		mVal = static_cast<unsigned int>(v % umod());
+	}
+
+	unsigned int val() const {
+		return mVal;
+	}
+
+	modint& operator++() {
+		mVal++;
+		if (mVal == umod()) {
+			mVal = 0;
+		}
+		return *this;
+	}
+
+	modint& operator--() {
+		if (mVal == 0) {
+			mVal = umod();
+		}
+		mVal--;
+		return *this;
+	}
+
+	modint operator++(int) {
+		modint result = *this;
+		++*this;
+		return result;
+	};
+
+	modint operator--(int) {
+		modint result = *this;
+		--*this;
+		return result;
+	}
+
+	modint& operator+=(const modint& rhs) {
+		mVal += rhs.mVal;
+		if (mVal >= umod()) {
+			mVal -= umod();
+		}
+		return *this;
+	}
+
+	modint& operator-=(const modint& rhs) {
+		mVal -= rhs.mVal;
+		if (mVal >= umod()) {
+			mVal += umod();
+		}
+		return *this;
+	}
+
+	modint& operator*=(const modint& rhs) {
+		unsigned long long z = mVal;
+		z *= rhs.mVal;
+		mVal = static_cast<unsigned int>(z % umod());
+		return *this;
+	}
+
+	modint& operator/=(const modint& rhs) {
+		mVal /= rhs.mVal;
+		return *this;
+	}
+
+	modint operator+() const {
+		return *this;
+	}
+
+	modint operator-() const {
+		return modint() - *this;
+	}
+
+	modint operator+(const modint& rhs) const {
+		return modint(*this) += rhs;
+	}
+
+	modint operator-(const modint& rhs) const {
+		return modint(*this) -= rhs;
+	}
+
+	modint operator*(const modint& rhs) const {
+		return modint(*this) *= rhs;
+	}
+
+	modint operator/(const modint& rhs) const {
+		return modint(*this) /= rhs;
+	}
+
+	bool operator==(const modint& rhs) const {
+		return mVal == rhs.mVal;
+	}
+
+	bool operator!=(const modint& rhs) const {
+		return mVal != rhs.mVal;
+	}
+
+	bool operator<(const modint& rhs) const {
+		return mVal < rhs.mVal;
+	}
+
+	bool operator>(const modint& rhs) const {
+		return mVal > rhs.mVal;
+	}
+
+	friend istream& operator>>(istream &in, modint &rhs) {
+		in >> rhs.mVal;
+		return in;
+	}
+
+	friend ostream& operator<<(ostream &out, const modint &rhs) {
+		out << rhs.mVal;
+		return out;
+	}
+
+	modint pow(long long int n) const {
+		assert(0 <= n);
+		modint x = *this;
+		modint r = 1;
+		while (n) {
+			if (n & 1) {
+				r *= x;
+			}
+			x *= x;
+			n >>= 1;
+		}
+
+		return r;
+	}
+};
+
+using mint = modint<1000000007, int>;
+
+class Solve {
+private:
+	int N;
+	vector<int> X;
+	int dp[201][201];
+
+	mint run(int idx, int start) {
+		if (idx == N - 1) {
+			return X[idx] - start + 1;
+		}
+
+		if (dp[idx][start] != -1) {
+			return dp[idx][start];
+		}
+
+		mint ret = 0;
+		for (int n = start; n <= X[idx]; n++) {
+			ret += run(idx + 1, n);
+		}
+
+		dp[idx][start] = static_cast<int>(ret.val());
+		return ret;
+	}
+
+public:
+	Solve () {}
+
+	void input() {
+		cin >> N;
+		X.resize(N);
+		for (int i = 0; i < N; i++) {
+			cin >> X[i];
+		}
+	}
+
+	void solve() {
+		memset(dp, -1, sizeof(dp));
+		sort(X.begin(), X.end());
+		cout << run(0, 1) * N << '\n';
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	Solve s;
+
+	int nTestcases;
+	cin >> nTestcases;
+	while (nTestcases--) {
+		s.input();
+		s.solve();
+	}
+
+	return 0;
+}
